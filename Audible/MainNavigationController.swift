1f3986fb2c1a6cc2028d544a9c91de32bb0a9837
Nick Kulakov <devshot777@gmail.com>
2017-12-30 00:50:26 +0300

    Swift 3: Audible - Hijacking Root View Controller from AppDelegate (Ep 8)

---
+++ Audible/MainNavigationController.swift
@@ -0,0 +1,46 @@
+//
+//  MainNavigationController.swift
+//  Audible
+//
+//  Created by Peter on 12/29/17.
+//  Copyright © 2017 Nick. All rights reserved.
+//
+
+import UIKit
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        if isLoggedIn() {
+            // assume user is logged in
+            let homeController = HomeController()
+            viewControllers = [homeController]
+        } else {
+            perform(#selector(showLoginController), with: nil, afterDelay: 0.01)
+            
+        }
+    }
+    
+    fileprivate func isLoggedIn() -> Bool {
+        return false
+    }
+    
+    @objc func showLoginController() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: {
+            
+        })
+    }
+    
+    class HomeController: UIViewController {
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            //view.backgroundColor = UIColor.yellow
+            navigationItem.title = "We're logged in"
+            let imageView = UIImageView(image: UIImage(named: "home"))
+            view.addSubview(imageView)
+            _ = imageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 64, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        }
+    }
+}
